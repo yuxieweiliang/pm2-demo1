e245a190862e88e8ef3c47b5d82675aee6d1f719
雨歇微凉 <1413074908@126.com>
2020-03-14 16:34:13 +0800

    change

---
+++ ecosystem.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  apps: [{
+    name: "pm2Demo",
+    script: "index.js",
+    // 默认情况下，PM2会监听应用是否ready，如果3000ms后没有ready会强制重载。 使用listen_timeout改变这个值。
+    wait_ready: true,
+    // 默认情况下，PM2在发送SIGKILL信号之前，如果应用进程没用自己退出，PM2等待1600毫秒后会主动杀掉应用进程。
+    kill_timeout: 1600,
+  }],
+  deploy: {
+    // "production" is the environment name
+    production: {
+      // SSH key path, default to $HOME/.ssh
+      key: "/path/to/some.pem",
+      // SSH user
+      user: "root",
+      // SSH host
+      host: ["39.107.87.157"],
+      // SSH options with no command-line flag, see 'man ssh'
+      // can be either a single string or an array of strings
+      ssh_options: "StrictHostKeyChecking=no",
+      // GIT remote/branch
+      ref: "origin/master",
+      // GIT remote
+      repo: "git@github.com:Username/repository.git",
+      // path in the server
+      path: "/var/www/my-repository",
+      // Pre-setup command or path to a script on your local machine
+      // 'pre-setup': "apt-get install git ; ls -la",
+      // Post-setup commands or path to a script on the host machine
+      // eg: placing configurations in the shared dir etc
+      'post-setup': "ls -la",
+      // pre-deploy action 预部署
+      'pre-deploy-local': "echo 'This is a local executed command'",
+      // post-deploy action 部署后执行
+      'post-deploy': "npm install",
+    },
+  }
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const Koa = require('koa2');
+
+const port = 8083;
+const app = new Koa();
+
+app.listen(port, function (err) {
+  if(err) return;
+  console.log(`listen for port ${port}`)
+});
+process.on('SIGINT', () => {
+  console.info('SIGINT signal received.')
+
+  // Stops the server from accepting new connections and finishes existing connections.
+  server.close(function(err) {
+    // if error, log and exit with error (1 code)
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    // close your database connection and exit with success (0 code)
+    // for example with mongoose
+    mongoose.connection.close(function () {
+      console.log('Mongoose connection disconnected')
+      process.exit(0)
+    })
+  })
+})
